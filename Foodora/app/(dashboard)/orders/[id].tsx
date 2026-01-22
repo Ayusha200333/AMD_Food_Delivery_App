a51a62241f04d6bbc761f16bd5d39fed5bcd9647
Ayusha200333 <ayushawijerathna@gmail.com>
2026-01-22 20:34:00 +0530

    add order id

---
+++ Foodora/app/(dashboard)/orders/[id].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const OrderDetails = () => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text>Order Details - View Full Order</Text>
+    </View>
+  )
+}
+
+export default OrderDetails
