2e05a5ded6891a6846a6b0618d42cbd967c5fbde
Ayusha200333 <ayushawijerathna@gmail.com>
2026-01-22 20:35:43 +0530

    add app layout

---
+++ Foodora/app/_layout.tsx
@@ -0,0 +1,22 @@
+import { View, Text } from "react-native"
+import React from "react"
+import { Slot } from "expo-router"
+import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context"
+import { LoaderProvider } from "@/context/LoaderContext"
+import { AuthProvider } from "@/context/AuthContext"
+
+const RootLayout = () => {
+  const insets = useSafeAreaInsets()
+
+  return (
+    <LoaderProvider>
+      <AuthProvider>
+        <View style={{ marginTop: insets.top, flex: 1 }}>
+          <Slot />
+        </View>
+      </AuthProvider>
+    </LoaderProvider>
+  )
+}
+
+export default RootLayout
