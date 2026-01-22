b4c84e70d438aec4c6026872b0ad8f1fa89610f5
Ayusha200333 <ayushawijerathna@gmail.com>
2026-01-22 20:30:40 +0530

    add food id

---
+++ Foodora/app/(dashboard)/foods/[id].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const FoodDetails = () => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text>Food Details - View Full Info</Text>
+    </View>
+  )
+}
+
+export default FoodDetails
